cf5d404150f8cc7d2f2b732b1c8311b41a601a4f
Fajas Internacionales <fajasinternacionales@iMac-de-Fajas.local>
2019-03-29 17:07:04 -0500

    deshabilitando Cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+var algoliasearch = require('algoliasearch');
+
 admin.initializeApp();
+
+const algoliaClient = algoliasearch('AKE68Y9274', 'f8604ef41d20eb78f752a3f55d935700')
+const clientsIndex = algoliaClient.initIndex('clients') 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -8,9 +13,22 @@ admin.initializeApp();
 //  response.send("Hello from Firebase!");
 // });
 
-const CLIENTS = '/clients'
+/* const CLIENTS = '/clients'
 
 exports.createClient = functions.firestore.document(`${CLIENTS}/{id}`)
     .onCreate((snapshot, context)=>{
-        console.log(snapshot.data(), context.params.id)
+        const actualClient = snapshot.data()
+        return clientsIndex.addObject({
+            name: actualClient.name,
+            country: actualClient.country.translations.es || actualClient.country.name,
+            seller: actualClient.seller,
+            id: context.params.id
+        })
+        .then(reuslt =>{
+            return snapshot.ref.update({algoliaId: reuslt.objectID})
+        })
+        .catch(err => {
+            console.log(err)
+        })
     })
+ */
