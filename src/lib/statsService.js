c2f76fff8abd61e80d1e2aec423c491fc08d9e18
Fabian David Dueñas <fabian0896@outlook.com>
2021-02-10 17:52:36 -0500

    arreglando estadisticas

--- src/lib/statsService.js
+++ src/lib/statsService.js
@@ -11,22 +11,34 @@ import numeral from 'numeral'
 
 const STATS = 'stats'
 const ORDERS = 'orders'
+const CLIENTS = 'clients'
+
 
 
 export async function getYearStats(year){
 
+    //Descomentat las siguientes lineas para actualizar las estadisticas basada en todos los pedidos
+    //usar esto en caso de que fallen las cloud functions para ajustar las estadisticas a lso valores reales
     const dateStart = moment().year(year).month(0).date(1).hour(0).minute(0).second(0)
     const dateEnd = moment().year(year + 1).month(0).date(0).hour(23).minute(59).second(59)
-
     console.log(dateStart.toDate(), dateEnd.toDate())
-
-
-    const ordersSnap = await firebase.firestore().collection(ORDERS).where('createdAt', '>=', dateStart.toDate()).where('createdAt', '<=', dateEnd.toDate()).limit(5).get()
-
+    const ordersSnap = await firebase.firestore().collection(ORDERS).where('createdAt', '>=', dateStart.toDate()).where('createdAt', '<=', dateEnd.toDate()).get()
     const ordersDataList = ordersSnap.docs.map(v => v.data())
+    const newResult = await formatDataOrders(ordersDataList)
+    const  statsByMonth = await getStatsArrayByMonth(ordersDataList)
+    const batch = firebase.firestore().batch()
+    statsByMonth.forEach((data,index)=>{
+        const ref = firebase.firestore().collection(STATS).doc(`${year}-${index+1}`)
+        batch.set(ref, {...data, year: year, month: index+1})
+    })
+    await batch.commit()
+    await firebase.firestore().collection(STATS).doc(`${year}`).set(newResult)
+    console.log("Se actualizaron las estadisticas")
 
-    const newResult = formatDataOrders(ordersDataList)
 
+
+
+    //desde aqui empieza el funcionamiento normal 
     const db = firebase.firestore().collection(STATS).doc(`${year}`)
     const snap = await db.get()
     if(!snap.exists){
@@ -34,14 +46,13 @@ export async function getYearStats(year){
     }
     const result = snap.data()
 
-    console.log(newResult)
     console.log(result)
 
     return {
         ...result,
         income:{
-            COP: result.income? thousandSeparator((result.income.COP || 0), true): 0,
-            USD: result.income? thousandSeparator((result.income.USD || 0), true): 0
+            COP: result.income? numeral(result.income.COP || 0).format("$0,0"): 0,
+            USD: result.income? numeral(result.income.USD || 0).format("$0,0") : 0
         }
     }
 }
@@ -76,7 +87,18 @@ export async function getMonthStats(year, month){
 //------------------ FUNCION PARA SCAR LAS ESTADISTICAS DE LOS PEDIDO ------------------
 
 
-const formatDataOrders = (orderList=[]) =>{
+const formatDataOrders = async (orderList=[]) =>{
+
+    //obtener todos los clientes
+    const clientSnap = await firebase.firestore().collection(CLIENTS).get()
+    const clientList = clientSnap.docs.reduce((prev, curr)=> {
+        return {
+            ...prev,
+            [curr.id]: curr.data()
+        }
+    }, {})
+
+
 
     //obtener el numero de ordenes totales
     const totalOrders = orderList.length
@@ -95,7 +117,7 @@ const formatDataOrders = (orderList=[]) =>{
 
         const totalValue = Object.keys(order.payments).map(id => parseFloat(order.payments[id].value)).reduce((p,c)=> p + c, 0)
 
-        if(order.currency == "COP"){
+        if(order.currency === "COP"){
             return {
                 ...prev,
                 COP: prev.COP + totalValue
@@ -150,8 +172,52 @@ const formatDataOrders = (orderList=[]) =>{
 
 
     //calculo de semanas weeks
+    const weeks = {}
+    orderList.forEach(order=>{
+        const orderDate = moment(new Date(order.createdAt.seconds * 1000))
+        const orderWeek = orderDate.week()
 
-    
+        if(!weeks[orderWeek]){
+            weeks[orderWeek]={
+                totalOrders: 1,
+                totalProducts: order.products.reduce((p,c)=> p + parseInt(c.quantity), 0)
+            }
+            return
+        }
+        weeks[orderWeek] = {
+            totalProducts: weeks[orderWeek].totalProducts + order.products.reduce((p,c)=> p + parseInt(c.quantity), 0),
+            totalOrders: weeks[orderWeek].totalOrders + 1
+        }
+
+    })
+
+
+
+    //agrupacion por cliente
+    const clients = {}
+    const countries = {}
+    orderList.forEach(order=>{
+        if(!clients[order.clientId]){
+            clients[order.clientId] = {
+                id: order.clientId,
+                name: clientList[order.clientId].name,
+                quantity: 1
+            }
+        }else{
+            clients[order.clientId].quantity += 1
+        }
+
+        const actualClient = clientList[order.clientId]
+        if(!countries[actualClient.country.alpha3Code]){
+            countries[actualClient.country.alpha3Code] = {
+                name: actualClient.country.translations.es,
+                quantity: 1
+            }
+        }else{
+            countries[actualClient.country.alpha3Code].quantity += 1
+        }
+
+    })
 
 
 
@@ -161,6 +227,32 @@ const formatDataOrders = (orderList=[]) =>{
         income,
         products,
         sizes,
+        weeks,
+        clients,
+        countries
     }
 
+}
+
+
+
+const getStatsArrayByMonth = async (orderList = [])=>{
+    
+
+    const dataMonth = {}
+    orderList.forEach(order=>{
+        const orderMont = moment(order.createdAt.seconds * 1000).month()
+        if(!dataMonth[orderMont]){
+            dataMonth[orderMont] = [order]
+        }else{
+            dataMonth[orderMont] = [...dataMonth[orderMont], order]
+        }
+    })
+
+    const stats = []
+    for(let month in dataMonth){
+        const result  = await formatDataOrders(dataMonth[month])
+        stats.push(result)
+    }
+    return stats
 }
